4d4b00f483083e72a824844b413976441ee27538
Nicholas Kachur <nick.e.kachur@gmail.com>
2015-01-29 19:01:41 -0800

    Normalize file permissions

--- setup_configs.sh
+++ setup_configs.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
+
 # Setup Vundle for Vim
-vim -u ~/.vim/bundles.vim +BundleInstall +q
+#vim -u ~/.vim/bundles.vim +BundleInstall +q
 
 # Setup symlinks for home directory (if required)
 
-# Check for existing configs
-# Ask for overwrite/save/etc.
-# If save, save them to .old_configs/<file>.old
-# Create symlinks for conky, vim, bash, etc.
+
+# Utility function to link each config file
+link_config_file() {
+    local filename=$1
+
+    if [[ exists "~/.$filename" ]]; then
+        echo -n "$filename already exists, overwrite? (y/n) "
+        read user_input
+    fi
+
+    if [[ "user_input" == "y" ]]; then
+        mv "~/.$filename" "~/.$filename.bak"
+        echo "Created backup file: "~/.$filename.bak"
+    else
+        echo "Skipping ~/.$filename"
+    fi
+}
+
+
+
+
+
+        
