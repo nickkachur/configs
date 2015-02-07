086eb3bbfa8052bc0d1e84448acefa72ab136d2c
Nicholas Kachur <nick.e.kachur@gmail.com>
2015-02-07 15:10:47 -0800

    Finish initial setup_configs script

--- setup_configs.sh
+++ setup_configs.sh
@@ -1,31 +1,44 @@
 #!/bin/bash
 
-
-# Setup Vundle for Vim
-#vim -u ~/.vim/bundles.vim +BundleInstall +q
-
 # Setup symlinks for home directory (if required)
 
+main() {
+    config_dir=$(dirname $0)
+
+    for file in $(ls $config_dir); do
+        if [[ "$file" == "README.md" || "$file" == "setup_configs.sh" ]]; then
+            # Skip Readme and this script (;
+            continue
+        else
+            link_config_file $file $config_dir
+        fi
+    done
+}
 
 # Utility function to link each config file
 link_config_file() {
     local filename=$1
+    local pathname=$2
 
-    if [[ exists "~/.$filename" ]]; then
-        echo -n "$filename already exists, overwrite? (y/n) "
+    local dotfile="$HOME/.$filename"
+    
+    # Prompt for user input if file already exists
+    # Must check for symlinks explicitly as dead symlinks don't exist
+    if [[ -e "$dotfile" || -L "$dotfile"  ]]; then
+        echo -n "Warning: $dotfile already exists, overwrite? (y/n) "
         read user_input
+        if [[ "$user_input" == "y" || "$user_input" == "Y" ]]; then
+            mv "$dotfile" "$dotfile.bak"
+            echo "Info:   Created backup file: '$dotfile.bak'"
+        else
+            echo "Info:   Skipping '$dotfile'"
+            return
+        fi
     fi
 
-    if [[ "user_input" == "y" ]]; then
-        mv "~/.$filename" "~/.$filename.bak"
-        echo "Created backup file: "~/.$filename.bak"
-    else
-        echo "Skipping ~/.$filename"
-    fi
+    echo "Info:   Creating link for '$dotfile' -> '$pathname/$filename'"
+    ln -s "$pathname/$filename" "$HOME/.$filename" 
 }
 
+main
 
-
-
-
-        
